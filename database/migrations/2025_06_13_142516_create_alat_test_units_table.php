44fabdf3d2227c4a5f7bb4aff319ad69aa16d4a9
faizalyagami <faizalyagami@gmail.com>
2025-06-13 15:14:36 +0700

    add migration alat test unit table

---
+++ database/migrations/2025_06_13_142516_create_alat_test_units_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlatTestUnitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alat_test_units', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('alat_test_id')->constrained()->onDelete('cascade');
+            $table->string('serial_number')->unique();
+            $table->enum('status', ['tersedia', 'dipinjam'])->default();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alat_test_units');
+    }
+}
